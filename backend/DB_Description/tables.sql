337f25c806fa388b4adb9212616ec1a71a9565cb
cheeseTotopos <hshernandez406@gmail.com>
2026-05-27 18:10:22 -0600

    Starting the api

---
+++ backend/DB_Description/tables.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE tasksSystem;
+USE tasksSystem;
+
+CREATE TABLE Companies(
+	Id int,
+    CompanyName varchar(255),
+    Pwd varchar(255),
+    IsActive int,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE Users(
+	Id int,
+    FullName varchar(255),
+    Email varchar(255),
+    CreatedAt date,
+    Companie int,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Companie) REFERENCES Companies(Id)
+);
+
+CREATE TABLE TaskStatus(
+	Id int,
+    StatusName varchar(255),
+    IsActive int,
+    Companie int,
+    CreatedBy int,
+    
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Companie) REFERENCES Companies(Id),
+    FOREIGN KEY (CreatedBy) REFERENCES Users(Id)
+);
+
+CREATE TABLE Tasks(
+	Id int,
+    Title varchar(255),
+    TaskDescription varchar(255),
+    StatusId int,
+    IsPriority int,
+    DueDate date,
+    CreatedBy int,
+    UserId int,
+    
+    PRIMARY KEY (Id),
+    FOREIGN KEY (StatusId) REFERENCES TaskStatus(Id),
+    FOREIGN KEY (CreatedBy) REFERENCES Users(Id),
+    FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
---
+++ backend/README.md
@@ -0,0 +1,2 @@
+On this first commit I just created the webapi with the command dotnet new webapi -o apiname.
+Also added a folder named DB_Description where i show the definition of the database and the tables.
